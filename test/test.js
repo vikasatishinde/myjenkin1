833623f917ef92a851f62ae9fb07813315ac1af3
vikasatishinde <vikasatishinde>
2023-10-15 12:29:16 +0000

    second

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var request = require('supertest');
 var app = require('../index.js');describe('GET /', function() {
 it('respond with Hello Jenkins', function(done) { //navigate to root and check the the response 
-is "hello world"
+is "hello vikas from world"
 request(app).get('/').expect('Hello Jenkins', done);
 });
 });
